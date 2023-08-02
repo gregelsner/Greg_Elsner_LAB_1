c31e7e7e85aa491ffdffe40d185bafc0c79144cc
gregelsner <gregelsner2019@gmail.com>
2023-08-02 08:12:20 +0000

    commint 2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,44 @@
-// The error below is generated when the code is downloaed into Mbed. It indicates that in ISR we should avoid any call
-// to bulky library functions in particullar certain library function like printf, these are non re-entrant 
-// librarys and their behaviour could be corrupted or not function as intended.
-// ++ MbedOS Error Info ++
-// Error Status: 0x80010133 Code: 307 Module: 1
-// Error Message: Mutex: 0x200015FC, Not allowed in ISR context
-// Location: 0x8009355
-// Error Value: 0x200015FC
-// Current Thread: rtx_idle Id: 0x20001D2C Entry: 0x8006DBD StackSize: 0x300 StackMem: 0x20001DF8 SP: 0x2009FF54 
+
+//Comment: When added veriable flag0 to interrupt function,
+//the output is very unstable and always indicates that the button is being pressed(falling edge) regardless of the physical button state. 
+//the flag is set to 1 and and not being reset  
+//proposed solution is to use switch statement with two cases to trigger desired interrupt function in a stable state, execute the code within the function in this case 
+//print the button state next to reset state of the flag0 veriable and break/exit the case fuction. This solution also allows to avoid switch debounce effect. 
 
 
 #include "mbed.h"
 
-// declare variables and Interrupt method
-InterruptIn button (PC_13); //Active high button on mbed board
+//declare variables and Interrupt method
+InterruptIn button (PC_13); //Active high (3.3VDC) button on mbed board
+char flag0=0; //Veriable of char type
 
-void button_interrupt(){ //Creating interrupt function
-printf("Button Pressed \r\n");//Printing mesage on serial coolterm interface
+void fall_int(){ //Declaring fall function 
+flag0=1;
 }
-int main(){ // main runs in its own thread in the OS
-button.fall (&button_interrupt);// If button on falling edge button_interrupt function is true and prints msg
+void rise_int(){ //Declaring rise function 
+flag0=2;
+}
+int main(){ //main runs in its own thread in the OS
+
+    button.fall(&fall_int);//When button is pressed voltage falling to 0VDC and sets the  fall_int function 
+    button.rise(&rise_int);//When button is depressed voltage rising to 3.3VDC and sets the rise_int function
+
+    while(true){ //Continous loop
+    switch (flag0){ //Switch statement compares flag0 state with case1&case2 and executes the code within the case
+    case 1:
+    printf("Fall Interrupt\n\r");
+    flag0=0;
+    break;
+    case 2:
+    printf("Rise Interrupt\n\r");
+    flag0=0;
+    break;
+    default:
+    flag0=0;
+    break;
+    
+    }
+ thread_sleep_for(50);
+}
 
 }
